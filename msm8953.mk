4fa5cc65b0cfd87393c4dc0cdf09c992fea4dbc1
Ganesh Varma <ganeshvarmai4u@gmail.com>
2018-09-22 13:32:00 +0530

    msm8953-common: discard snap for tissot
    * temp

--- msm8953.mk
+++ msm8953.mk
@@ -120,9 +120,13 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
+    
+ifneq ($(TARGET_DEVICE),tissot)    
+PRODUCT_PACKAGES += \
+    Snap 
+endif   
 
 # Configstore
 PRODUCT_PACKAGES += \
