0f8fffac2b898d4e7f7f030229f6627592574dd8
Amulya Bhardwaj <35401003+AmulyaX@users.noreply.github.com>
2018-12-12 14:41:48 +0530

    Remove duplicate usage of Doze

--- msm8953.mk
+++ msm8953.mk
@@ -123,10 +123,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
