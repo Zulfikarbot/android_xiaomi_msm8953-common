d363b73048921f3875c770ab5db8d69239a7591a
Flex1911 <dedsa2002@gmail.com>
2018-12-20 10:56:23 +0100

    vince: Disable debug.sf.latch_unsignaled
    Apparently, it introduces dramatical performance degradation in some use cases.
    
    vince: Disable sys.use_fifo_ui
    
    This was a part of Xiaomi's ricing in P.
    While it generally should improve UI performance, it looks like it
    does exactly the opposite for us.

--- prop.mk
+++ prop.mk
@@ -71,20 +71,25 @@ persist.debug.coresight.config=stm-events
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.egl.hw=0 \
-debug.enable.sglscale=1 \
-debug.gralloc.enable_fb_ubwc=1 \
-debug.mdpcomp.logs=0 \
-debug.sf.hw=0 \
-dev.pm.dyn_samplingrate=1 \
-persist.debug.wfd.enable=1 \
-persist.demo.hdmirotationlock=false \
-persist.hwc.enable_vds=1 \
-persist.hwc.mdpcomp.enable=true \
-ro.opengles.version=196610 \
-ro.qualcomm.cabl=0 \
-ro.sf.lcd_density=480
-
+	debug.sf.enable_hwc_vds=1 \
+	debug.egl.hw=0 \
+	debug.enable.sglscale=1 \
+	debug.gralloc.enable_fb_ubwc=1 \
+	debug.mdpcomp.logs=0 \
+	debug.sf.hw=0 \
+	debug.sf.recomputecrop=0 \
+	dev.pm.dyn_samplingrate=1 \
+	persist.demo.hdmirotationlock=false \
+	persist.hwc.mdpcomp.enable=true \
+	ro.opengles.version=196610 \
+	ro.qualcomm.cabl=0 \
+	ro.sf.lcd_density=440 \
+	ro.vendor.display.cabl=2 \
+	sdm.debug.disable_skip_validate=1 \
+	vendor.display.disable_skip_validate=1 \
+	vendor.display.enable_default_color_mode=1 \
+	vendor.gralloc.enable_fb_ubwc=1	
+	
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
 drm.service.enabled=true
